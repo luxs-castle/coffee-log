2c2af279795165e0850405fd1a6811198377d450
Thomas Gilgenast <thomasgilgenast@gmail.com>
2017-01-22 18:30:35 -0500

    added manuel:viewmodel package
    renamed Brew collection to Brews
    
    used viewmodel to add two-way binding to brew component,
    greatly simplifying code and fixing many issues related to DOM redraws
    
    deleted brew/stage component as part of brew component simplification
    
    changed app behavior to force navigation to log view after submitting brew form
    
    brew stages are now displayed as a table in the log
    
    deleted unused log/stage component

--- imports/api/brew/brew.js
+++ imports/api/brew/brew.js
@@ -1,16 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
-export const Brew = new Mongo.Collection('brew');
+export const Brews = new Mongo.Collection('brew');
 
 Meteor.methods({
   'brew.create'(bean, apparatus, grindSetting, beanMass, waterMass, waterTemp, stages, tastingNotes, techNotes){
 
-    Brew.insert({
+    Brews.insert({
       createdAt: new Date(),
       owner: Meteor.userId(),
       username: Meteor.user().username,
-      bean, apparatus, grindSetting, beanMass, waterMass, waterTemp, stages, tastingNotes, techNotes
+      bean: bean,
+      apparatus: apparatus,
+      grindSetting: grindSetting,
+      beanMass: beanMass,
+      waterMass: waterMass,
+      waterTemp: waterTemp,
+      stages: stages,
+      tastingNotes: tastingNotes,
+      techNotes: techNotes
     });
   },
 })
