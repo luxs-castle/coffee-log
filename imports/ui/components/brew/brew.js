9d07d56e78978596818e9814cbbb3fa7179dc088
hzhao <heling.zhao@gmail.com>
2017-02-12 20:10:31 -0500

    Adding delete option for individual stages.

--- imports/ui/components/brew/brew.js
+++ imports/ui/components/brew/brew.js
@@ -3,6 +3,7 @@ import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import './brew.html';
+import './brew.css';
 
 Template.brew.viewmodel({
   apparatus: '',
@@ -13,20 +14,36 @@ Template.brew.viewmodel({
   waterTemp: '',
   tastingNotes: '',
   techNotes: '',
-  stages: [{name: 'Stage 1', water: '', time: ''}],
+  stages: [{name: 'Brew', water: '', time: ''}],
   onCreated: function() {
     // workaround for persistence bug where first stage values persist on nav
     this.stages().pop();
-    this.stages().push({name: 'Stage 1', water: '', time: ''});
+    this.stages().push({name: 'Brew', water: '', time: ''});
   },
   addStage: function(event) {
     event.preventDefault();
     this.stages().push(
       {name: 'Stage ' + (this.stages().length + 1), water: '', time: ''});
+    this.renameStages();
   },
-  removeStage: function(event) {
-    event.preventDefault();
-    this.stages().pop();
+  removeStage: function(stage) {
+    this.stages().remove(stage);
+    this.renameStages();
+  },
+  renameStages: function() {
+    if(this.stages().length == 1) {
+      this.stages()[0].name = 'Brew';
+    }
+    else if (this.stages().length == 2) {
+      this.stages()[0].name = 'De-Gas';
+      this.stages()[1].name = 'Brew';
+    }
+    else {
+      this.stages()[0].name = 'De-Gas';
+      for (let i = 1; i < this.stages().length; i++) {
+        this.stages()[i].name = 'Stage ' + i;
+      }
+    }
   },
   submitBrew: function(event) {
     event.preventDefault();
